494599937a1ccebaa49241020ee14b38ee078959
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-12-03 22:11:41 +0545

    used black for code formatting

--- task_cli.py
+++ task_cli.py
@@ -9,62 +9,66 @@ console = Console()
 app = typer.Typer()
 
 
-@app.command(short_help='adds an item')
+@app.command(short_help="adds an item")
 def add(task: str, category: str):
-	typer.echo(f"adding {task}, {category}")
-	
-	todo = Todo(task, category)
-	insert_todo(todo)	
+    typer.echo(f"adding {task}, {category}")
 
-	show()
+    todo = Todo(task, category)
+    insert_todo(todo)
+
+    show()
+
 
 @app.command()
 def delete(position: int):
-	typer.echo(f"deleting {position}")
+    typer.echo(f"deleting {position}")
 
-	delete_todo(position-1)
+    delete_todo(position - 1)
 
-	show()
+    show()
+
 
 @app.command()
 def update(position: int, task: str = None, category: str = None):
-	typer.echo(f"updating {position}")
+    typer.echo(f"updating {position}")
 
-	update_todo(position-1, task, category)
+    update_todo(position - 1, task, category)
 
-	show()	
+    show()
+
 
 @app.command()
 def complete(position: int):
-	typer.echo(f"complete {position}")
-	
-	complete_todo(position-1)
+    typer.echo(f"complete {position}")
 
-	show()
+    complete_todo(position - 1)
+
+    show()
+
 
 @app.command()
 def show():
-	tasks = get_all_todos() 	
-	console.print("[bold magenta]Todos[/bold magenta]!", "🖥️")
+    tasks = get_all_todos()
+    console.print("[bold magenta]Todos[/bold magenta]!", "🖥️")
 
-	table = Table(show_header=True, header_style="bold blue")
-	table.add_column("#", style="dim", width=6)
-	table.add_column("Todo", min_width=20)
-	table.add_column("Category", min_width=12, justify="right")
-	table.add_column("Done", min_width=12, justify="right")
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("#", style="dim", width=6)
+    table.add_column("Todo", min_width=20)
+    table.add_column("Category", min_width=12, justify="right")
+    table.add_column("Done", min_width=12, justify="right")
 
-	def get_category_color(category):
-		COLORS = {'Learn': 'cyan', 'Youtube': 'red', 'Sports': 'cyan', 'Study': 'green'}
-		if category in COLORS:
-			return COLORS[category]
-		return 'white'
+    def get_category_color(category):
+        COLORS = {"Learn": "cyan", "Youtube": "red", "Sports": "cyan", "Study": "green"}
+        if category in COLORS:
+            return COLORS[category]
+        return "white"
 
-	for idx, task in enumerate(tasks, start=1):
-		c = get_category_color(task.category)
-		is_done_str = '✅' if task.status == 2 else '❌'
-		table.add_row(str(idx), task.task, f'[{c}]{task.category}[/{c}]', is_done_str)
-	console.print(table)
+    for idx, task in enumerate(tasks, start=1):
+        c = get_category_color(task.category)
+        is_done_str = "✅" if task.status == 2 else "❌"
+        table.add_row(str(idx), task.task, f"[{c}]{task.category}[/{c}]", is_done_str)
+    console.print(table)
 
 
 if __name__ == "__main__":
-	app()
+    app()
